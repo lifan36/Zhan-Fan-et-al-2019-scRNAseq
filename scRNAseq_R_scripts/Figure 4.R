e9df890884c791bf7f2a013845e6e32313c0abfe
lifan36 <64649095+lifan36@users.noreply.github.com>
2020-07-31 08:43:44 -0400

    Update Figure 4.R

--- scRNAseq_R_scripts/Figure 4.R
+++ scRNAseq_R_scripts/Figure 4.R
@@ -1,25 +1,26 @@
 # Figure 4a
-# Script to make volcano plot for cluster 6 makers 
-# Cluster 6 is enriched for Mac2 + cell cluster and is  significantly enriched in D2 samples
 
 library(ggplot2)
 library(Seurat)
 library(ggrepel)
 library(RColorBrewer)
-
+library(dplyr)
+library(cowplot)
+library(reshape2)
+library(MAST)
+library(patchwork)
 # Load in Seurat object
 setwd("")
-data<-readRDS("elife_Ctrl_D0_D2.rds")
+data<-readRDS("elife_microglial_cells_only.rds")
+data@active.ident
 
-Idents(data) <- "seurat_clusters_new"
 
-# Find markers for Cluster 6:
-marker <- FindMarkers(data,6, logfc.threshold = 0,
-                      test.use = "MAST")
-
+# Find markers for Cluster 5:
+marker <- FindMarkers(data,5, logfc.threshold = 0, test.use = "MAST")
+write.csv(marker, "Cluster5_markers.csv")
 # Volcano plot of marker genes =====
 
-# Identify DEGs for cluster 6 markers
+# Identify DEGs for cluster 5 markers
 marker$colours <- c("NC")
 marker$colours[marker$avg_logFC >= 0.5 & marker$p_val_adj <= 0.05] <- c("UP")
 marker$colours[marker$avg_logFC <= -0.5 & marker$p_val_adj <= 0.05] <- c("DN")
@@ -29,7 +30,7 @@ genes_select_mature <- c("Cx3cr1", "Csf1r", "Mafb", "Tmem119")
 genes_to_plot_mature <- marker[row.names(marker) %in% genes_select_mature, ]
 genes_to_plot_mature$Cluster <- "Mature"
 
-genes_select_immature <- c("Mmp8", "Mmp9", "Ifitm1", "Lgals3", "Il1b")
+genes_select_immature <- c("Ifitm1", "Lgals3", "Il1b")
 genes_to_plot_immature <- marker[row.names(marker)  %in% genes_select_immature, ]
 genes_to_plot_immature$Cluster <- c("Immature")
 
@@ -57,7 +58,7 @@ ggplot() +
                   color="black", fontface = 'bold',size = 5, box.padding = 0.5,
                   point.padding = 0.5, segment.size=0.25, segment.colour="black") +
   ylab("-Log10[FDR]") + xlab("Log2FC") +
-  ggtitle("Cluster 6 vs all other clusters")+
+  ggtitle("Cluster 5 vs all other clusters")+
   theme_bw()+
   theme(panel.grid.major.x  = element_blank(),
         panel.grid.major.y  = element_blank(),
@@ -72,20 +73,20 @@ ggplot() +
   scale_x_continuous(breaks=seq(-4, 4, 2), limits=c(-4, 4))+
   NoLegend()
 
-ggsave("Volcano_cluster_6.pdf", plot = last_plot(), device = "pdf", path = "",
+ggsave("Volcano_cluster_5.pdf", plot = last_plot(), device = "pdf", path = "",
        scale = 0.8, width = 7, height = 7, units = c("in"),
        dpi = 600, limitsize = FALSE)
 
 # #Figure 4b
 #change the color code
-p1 <- FeaturePlot(LG180_integrated, features = c("Lgals3"), combine = FALSE )
+p1 <- FeaturePlot(LG180_integrated, features = c("Lgals3")) & 
+  theme( plot.title = element_text( face = "italic") )
 scale <- scale_color_gradientn(colours = brewer.pal(11,"RdYlBu")[11:1],limits=c(0,4))
 p2 <- lapply(p1, function (x) x + scale)
 CombinePlots(p2)
 
 #Figure 4c
-Cluster_5 <- subset(LG180_integrated, idents = "5")
-Cluster_6 <- subset(LG180_integrated, idents = "6")
-VlnPlot(Cluster_5, features = "Lgals3", group.by = "Condition", pt.size = 0.1, combine = FALSE)
-VlnPlot(Cluster_6, features = "Lgals3", group.by = "Condition", pt.size = 0.1, combine = FALSE)
+Cluster_5 <- subset(data, idents = "5")
+VlnPlot(Cluster_5, features = "Lgals3", group.by = "Condition", pt.size = 0.1) & 
+  theme( plot.title = element_text( face = "italic") )
 
