49abf1f7ea4801079e69a7c4ddfe87d614c4bf71
lifan36 <64649095+lifan36@users.noreply.github.com>
2020-10-24 17:41:46 -0400

    Update Figure 3.R

--- scRNAseq_R_scripts/Figure 3.R
+++ scRNAseq_R_scripts/Figure 3.R
@@ -10,6 +10,6 @@ setwd("")
 LG180_integrated <- readRDS("elife_microglial_cells_only.rds")
 
 #Figure 3 a-f
-Cluster_1_2 <- subset(LG180_integrated, idents = c("1", "2"))
+Cluster_2 <- subset(LG180_integrated, idents = "2")
 VlnPlot(Cluster_1_2, features = c("Tmem119","P2ry12","P2ry13", "Selplg", "Cx3cr1", "Csf1r"), group.by = "Condition", pt.size = 0, combine = T)& 
   theme( plot.title = element_text( face = "italic") )
