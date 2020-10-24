293f339ac697354c7871aed17e983b9ec7cf6274
lifan36 <64649095+lifan36@users.noreply.github.com>
2020-10-24 17:50:39 -0400

    Update Figure 4.R

--- scRNAseq_R_scripts/Figure 4.R
+++ scRNAseq_R_scripts/Figure 4.R
@@ -12,12 +12,12 @@ library(patchwork)
 # Load in Seurat object
 setwd("")
 data<-readRDS("elife_microglial_cells_only.rds")
-data@active.ident
+Idents(data) <- "Condition"
 
 
-# Find markers for Cluster 5:
-marker <- FindMarkers(data,5, logfc.threshold = 0, test.use = "MAST")
-write.csv(marker, "Cluster5_markers.csv")
+# Find markers for D0 vs Ctrl:
+marker <- FindMarkers(data, ident.1 = "D0", ident.2 = "Ctrl", logfc.threshold = 0, test.use = "MAST")
+write.csv(marker, "D0vsCtrl_markers.csv")
 # Volcano plot of marker genes =====
 
 # Identify DEGs for cluster 5 markers
@@ -26,11 +26,11 @@ marker$colours[marker$avg_logFC >= 0.5 & marker$p_val_adj <= 0.05] <- c("UP")
 marker$colours[marker$avg_logFC <= -0.5 & marker$p_val_adj <= 0.05] <- c("DN")
 
 # Selected genes to highlight
-genes_select_mature <- c("Cx3cr1", "Csf1r", "Mafb", "Tmem119")
+genes_select_mature <- c("Cx3cr1", "Csf1r", "Mafb", "Tmem119","P2ry12","ApoE")
 genes_to_plot_mature <- marker[row.names(marker) %in% genes_select_mature, ]
 genes_to_plot_mature$Cluster <- "Mature"
 
-genes_select_immature <- c("Ifitm1", "Lgals3", "Il1b")
+genes_select_immature <- c("Lgals3", "Il1b","Cd52","Cd74","Lyz2")
 genes_to_plot_immature <- marker[row.names(marker)  %in% genes_select_immature, ]
 genes_to_plot_immature$Cluster <- c("Immature")
 
@@ -58,7 +58,7 @@ ggplot() +
                   color="black", fontface = 'bold',size = 5, box.padding = 0.5,
                   point.padding = 0.5, segment.size=0.25, segment.colour="black") +
   ylab("-Log10[FDR]") + xlab("Log2FC") +
-  ggtitle("Cluster 5 vs all other clusters")+
+  ggtitle("D0 vs Ctrl")+
   theme_bw()+
   theme(panel.grid.major.x  = element_blank(),
         panel.grid.major.y  = element_blank(),
@@ -70,23 +70,20 @@ ggplot() +
         axis.title.y = element_text(colour = "black", size=15),
         plot.title = element_text(size = 15, face = "bold")) +
   theme(aspect.ratio = 1) +
-  scale_x_continuous(breaks=seq(-4, 4, 2), limits=c(-4, 4))+
+  scale_x_continuous(breaks=seq(-2, 2, 1), limits=c(-2, 2))+
   NoLegend()
 
-ggsave("Volcano_cluster_5.pdf", plot = last_plot(), device = "pdf", path = "",
+ggsave("Volcano_D0vsCtrl.pdf", plot = last_plot(), device = "pdf", path = "",
        scale = 0.8, width = 7, height = 7, units = c("in"),
        dpi = 600, limitsize = FALSE)
 
 # #Figure 4b
 #change the color code
-p1 <- FeaturePlot(LG180_integrated, features = c("Lgals3")) & 
+FeaturePlot(LG180_integrated, features = c("Lgals3")) & 
   theme( plot.title = element_text( face = "italic") )
-scale <- scale_color_gradientn(colours = brewer.pal(11,"RdYlBu")[11:1],limits=c(0,4))
-p2 <- lapply(p1, function (x) x + scale)
-CombinePlots(p2)
 
 #Figure 4c
-Cluster_5 <- subset(data, idents = "5")
-VlnPlot(Cluster_5, features = "Lgals3", group.by = "Condition", pt.size = 0.1) & 
+Cluster_4 <- subset(data, idents = "4")
+VlnPlot(Cluster_4, features = "Lgals3", group.by = "Condition", pt.size = 0.1) & 
   theme( plot.title = element_text( face = "italic") )
 
